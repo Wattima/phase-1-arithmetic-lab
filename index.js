7f04ad90185f14278369c53edd58a357b3319ec1
Daryl Wattima <darwattima@gmail.com>
2022-11-22 21:43:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2; 
+
+const random = parseInt(Math.random() * 10);
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const max = Math.max(18, 13, 2, 0, 20, 5);
